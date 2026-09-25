c60ef939019c7b1fd6d8eb5acc2c9986dbec85e1
GADDAM VINOD KUMAR <143624560+vinodgaddam1@users.noreply.github.com>
2026-09-25 18:57:28 +0530

    Update sem.sv

--- SystemVerilog/Semaphore/sem.sv
+++ SystemVerilog/Semaphore/sem.sv
@@ -1,6 +1,25 @@
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+                 SystemVerilog semaphores
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+1. Definition
+A semaphore is like a set of keys/tokens.
+•	A process must get a key before accessing a shared resource. 
+•	After finishing, it puts the key back. 
+•	If no key is available, the process waits. 
+SystemVerilog provides the built-in semaphore class.
 
 
+1. new()
+Creates the semaphore with a specified number of keys.
+2. get()
+Used to acquire keys.
+3. try_get()
+Used to acquire keys without waiting.
+4. put()
+Used to return/release keys.
 
+For Example:
 module top;
 semaphore sem=new(1);
 //semaphore sem=new(2);
@@ -29,32 +48,6 @@ initial begin
 	sem.put();
 end
 endmodule
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
